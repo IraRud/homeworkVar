314db4827c48ab12e48d54aecdf8cf0f98bef11e
rudskaya_irina <irarudskaya5@gmail.com>
2022-09-22 23:39:03 +0300

    Задача №1 из ДЗ-1 "Переменные. Часть 1"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача №1:");
+        double dog = 8;
+        double cat = 3.6;
+        int paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
     }
 }
