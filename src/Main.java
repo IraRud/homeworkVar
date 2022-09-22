bf15fc1bd9f72f074aeb35c55d55f40b978f0304
rudskaya_irina <irarudskaya5@gmail.com>
2022-09-22 23:47:08 +0300

    Задача №4 из ДЗ-2 "Переменные. Часть 1"

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Задача №1:");
-        double dog = 8;
-        double cat = 3.6;
-        int paper = 763789;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-
-        System.out.println("Задача №2:");
-        dog = dog + 4;
-        cat = cat + 4;
-        paper = paper + 4;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-
-        System.out.println("Задача №3:");
-        dog = dog - 3.5;
-        cat = cat - 1.6;
-        paper = paper - 7639;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
+        System.out.println("Задача №4:");
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
 
     }
 }
