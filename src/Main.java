3e84434f06bf7452886529513171a1579f874b54
rudskaya_irina <irarudskaya5@gmail.com>
2022-09-22 23:54:49 +0300

    Задача №8 из ДЗ-3 "Переменные. Часть 1"

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,24 @@ public class Main {
         var differenceWeight = weightSecondBoxer - weightFirstBoxer;
         System.out.println("Разница между весом боксеров " + differenceWeight + " кг.");
 
+
         System.out.println("Задача №7:");
         var differenceWeightMethodOne = weightSecondBoxer - weightFirstBoxer;
         System.out.println("Разница между весом боксеров " + differenceWeightMethodOne + " кг.");
         var differenceWeightMethodOTwo = weightSecondBoxer  % weightFirstBoxer;
         System.out.println("Разница между весом боксеров " + differenceWeightMethodOTwo + " кг.");
+
+
+        System.out.println("Задача №8:");
+        var hours = 640;
+        var hoursEmployee = 8;
+        var employees = hours / hoursEmployee;
+        System.out.println("Всего работников в компании " + employees + " человек.");
+
+        var additionalEmployees = 94;
+        employees = employees + additionalEmployees;
+        hours = employees * hoursEmployee ;
+        System.out.println("Если в компании работает " + employees + " человек, то всего " + hours + " часов работы может быть поделено между сотрудниками.");
+
     }
 }
