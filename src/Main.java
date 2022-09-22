e7a68307973d7ebf143d3fe532250e806a0d172b
rudskaya_irina <irarudskaya5@gmail.com>
2022-09-22 23:50:29 +0300

    Задача №5 из ДЗ-2 "Переменные. Часть 1"

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,15 @@ public class Main {
         friend = friend / 7;
         System.out.println(friend);
 
+        System.out.println("Задача №5:");
+        double frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
     }
 }
